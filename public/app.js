2d225c32625c7bba4ee9ab3b3bac45cd2de388fb
Ivo Rojas <ivorojasdev@gmail.com>
2020-12-23 22:49:23 -0400

    add firebase connection

---
+++ public/app.js
@@ -0,0 +1,69 @@
+
+const auth = firebase.auth();
+
+const whenSignedIn = document.getElementById('whenSignedIn');
+const whenSignedOut = document.getElementById('whenSignedOut');
+
+const signInBtn = document.getElementById('signInBtn');
+const signOutBtn = document.getElementById('signOutBtn');
+
+const userDetails = document.getElementById('userDetails');
+
+const provider = new firebase.auth.GoogleAuthProvider();
+
+signInBtn.onclick = () => auth.signInWithPopup(provider);
+
+signOutBtn.onclick = () => auth.signOut();
+
+auth.onAuthStateChanged(user => {
+    if (user) {
+        whenSignedIn.hidden = false;
+        whenSignedOut.hidden = true;
+        userDetails.innerHTML =
+            `<h3>Hello ${user.displayName}!</h3>` +
+            ` <p>User ID: ${user.uid}</p> <img src=${user.photoURL} />`;
+    } else {
+        whenSignedIn.hidden = true;
+        whenSignedOut.hidden = false;
+        userDetails.innerHTML = ``;
+    }
+});
+
+
+const db = firebase.firestore();
+
+const createThing = document.getElementById('createThing');
+const thingList = document.getElementById('thingsList');
+
+let thingsRef;
+let unsubscribe;
+
+auth.onAuthStateChanged(user => {
+    if (user) {
+        thingsRef = db.collection('things')
+
+        createThing.onclick = () => {
+            let name = faker.commerce.productName();
+            const { serverTimestamp } = firebase.firestore.FieldValue;
+
+            thingsRef.add({
+                uid: 'imposter',
+                name,
+                createdAt: serverTimestamp()
+
+            });
+
+            unsubscribe = thingsRef
+                .where('uid', '==', user.uid)
+                .orderBy('createdAt')
+                .onSnapshot(querySnapshot => {
+                    const items = querySnapshot.docs.map(doc => {
+                        return `<li> ${doc.data().name} </li>`
+                    })
+                    thingsList.innerHTML = items.join('\n');
+                })
+        }
+    } else {
+        unsubscribe && unsubscribe();
+    }
+});
